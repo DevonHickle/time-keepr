e0da779904dafee37e30010d238f5fe8da1596a8
Devon M. Hickle <devon.hickle@wnins.com>
2023-07-07 09:32:55 -0500

    fixing routes, adding initial datepicker config

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,6 +6,7 @@ import Dashboard from '../Dashboard/Dashboard'
 import Preferences from '../Preferences/Preferences'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import useToken from './useToken'
+import Datepicker from '../Datepicker/Datepicker'
 
 function App(){
     const {token, setToken} = useToken()
@@ -16,14 +17,10 @@ function App(){
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/dashboard">
-                <Dashboard />
-                </Route>
-            </Routes>
-            <Routes>
-                <Route path="/preferences">
-                <Preferences />
-                </Route>
+                <Route path="/" element={<Login />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/preferences" element={<Preferences />} />
+                <Route path="/datepicker" element={<Datepicker />} />
             </Routes>
         </BrowserRouter>
     )
