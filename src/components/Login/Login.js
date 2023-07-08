880acb543fea8f10b4ee6a5f91cd9d1a8b283d98
devonhickle <devonhickle@yahoo.com>
2023-07-07 20:11:39 -0500

    added auth method and sign up page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -14,6 +14,12 @@ async function loginUser(creds) {
 
 export default function Login({ setToken }) {
 
+    let [authMode, setAuthMode] = useState('signin')
+
+    const changeAuthMode = () => {
+        setAuthMode(authMode === 'signin' ? 'signup' : 'signin')
+    }
+
     const [username, setUserName] = useState()
     const [password, setPassword] = useState()
     const handleSubmit = async e=> {
@@ -24,37 +30,81 @@ export default function Login({ setToken }) {
         setToken(token)
     }
 
+    if (authMode === 'signin') {
+        return (
+            <div className='Auth-form-body'>
+                <div className='Auth-form-container'>
+                    <form className="Auth-form" onSubmit={handleSubmit}>
+                        <div className='Auth-form-content'>
+                            <h3 classname="Auth-form-title">Welcome to Time Keepr</h3>
+                            <h4 classname="Auth-form-subtitle">Please login to continue</h4>
+                            <div className='text-center'>Not registered?{" "}
+                                <span className='link-primary' onClick={changeAuthMode}>
+                                    Sign Up
+                                </span>
+                            </div>
+                                <label>Email Address</label>
+                                <input 
+                                type='text' 
+                                placeholder='Enter email'
+                                className="form-control mt-1"
+                                onChange={e => setUserName(e.target.value)} 
+                                />
+                                <label>Password</label>
+                                    <input 
+                                        type='password' 
+                                        placeholder='Enter password'
+                                        className="form-control mt-1"
+                                        onChange={e => setPassword(e.target.value)} 
+                                    />
+                                <div className='d-grid gap-2 mt-3'>
+                                    <button type='submit' className='btn btn-primary'>Submit</button>
+                                </div>
+                                <p className="forgot-password text-right">
+                                Forgot <a href="#">password?</a>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        )
+    }
     return (
         <div className='Auth-form-body'>
-            <div className='Auth-form-container'>
-                <form className="Auth-form" onSubmit={handleSubmit}>
-                    <div className='Auth-form-content'>
-                        <h3 classname="Auth-form-title">Welcome to Time Keepr</h3>
-                        <h4 classname="Auth-form-subtitle">Please login to continue</h4>
-                            <label>Email Address</label>
-                            <input 
-                            type='text' 
-                            placeholder='Enter email'
-                            className="form-control mt-1"
-                            onChange={e => setUserName(e.target.value)} 
-                            />
-                            <label>Password</label>
-                                <input 
-                                    type='password' 
-                                    placeholder='Enter password'
-                                    className="form-control mt-1"
-                                    onChange={e => setPassword(e.target.value)} 
-                                />
-                            <div className='d-grid gap-2 mt-3'>
-                                <button type='submit' className='btn btn-primary'>Submit</button>
+                <div className='Auth-form-container'>
+                    <form className="Auth-form" onSubmit={handleSubmit}>
+                        <div className='Auth-form-content'>
+                            <h3 classname="Auth-form-title">Time Keepr</h3>
+                            <h4 classname="Auth-form-subtitle">Create an account</h4>
+                            <div className='text-center'>Already Registered?{" "}
+                                <span className='link-primary' onClick={changeAuthMode}>
+                                    Sign In
+                                </span>
                             </div>
-                            <p className="forgot-password text-right">
-                            Forgot <a href="#">password?</a>
-                        </p>
-                    </div>
-                </form>
+                                <label>Email Address</label>
+                                <input 
+                                type='text' 
+                                placeholder='Enter email'
+                                className="form-control mt-1"
+                                onChange={e => setUserName(e.target.value)} 
+                                />
+                                <label>Password</label>
+                                    <input 
+                                        type='password' 
+                                        placeholder='Enter password'
+                                        className="form-control mt-1"
+                                        onChange={e => setPassword(e.target.value)} 
+                                    />
+                                <div className='d-grid gap-2 mt-3'>
+                                    <button type='submit' className='btn btn-primary'>Submit</button>
+                                </div>
+                                <p className="forgot-password text-right">
+                                Forgot <a href="#">password?</a>
+                            </p>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
     )
 }
 
