2f692180126d7150053f886b599605b89e0990de
Devon M. Hickle <devon.hickle@wnins.com>
2023-07-17 13:17:26 -0500

    adding database for auth

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import './App.css'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Login from '../Login/Login'
@@ -7,6 +7,9 @@ import Preferences from '../Preferences/Preferences'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import useToken from './useToken'
 import Datepicker from '../Datepicker/Datepicker'
+import OTPInput from '../Login/OTPInput'
+import ForgotPassword from '../Login/ForgotPassword'
+export const RecoveryContext = createContext();
 
 export default function App(){
     const {token, setToken} = useToken()
@@ -18,20 +21,19 @@ export default function App(){
         if (page === 'login') return <Login />;
         if (page === 'otp') return <OTPInput />;
         if (page === 'forgotpassword') return <ForgotPassword />;
+    }
 
     if(!token) {
         return <Login setToken={setToken} />
     }
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/preferences" element={<Preferences />} />
-                <Route path="/datepicker" element={<Datepicker />} />
-                <Route path="/forgotpassword" element={<ForgotPassword />} />
-            </Routes>
-        </BrowserRouter>
-        )
-    }
+        <div className="App-header"> 
+          <RecoveryContext.Provider
+            value={{ page, setPage, otp, setOTP, username, setUsername }}>
+            <div>
+              <NavigateComponents />
+            </div>
+          </RecoveryContext.Provider>
+        </div>
+      );
 }
